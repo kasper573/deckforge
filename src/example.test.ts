d624f8fe0a759af8c81c40e7f74b0b64b879c3cf
ksandin <kasper.sandin@gmail.com>
2022-11-10 22:14:46 +0100

    Rename example unit test suite to shared/deckforge.ts

--- src/example.test.ts
+++
@@ -1,7 +0,0 @@
-import { add } from "./example";
-
-describe("example", () => {
-  it("can get hello from tRPC", () => {
-    expect(add(1, 2)).toBe(3);
-  });
-});
---
+++ src/shared/deckforge.test.ts
@@ -0,0 +1,7 @@
+import { add } from "./deckforge";
+
+describe("deckforge", () => {
+  it("can unit test", () => {
+    expect(add(1, 2)).toBe(3);
+  });
+});
