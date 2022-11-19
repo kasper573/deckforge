5a705d5bc5a69b88618ac95d5b08a43cfbb00e76
ksandin <kasper.sandin@gmail.com>
2022-11-19 01:26:10 +0100

    Add winningPlayerId to Battle

--- src/shared/deckforge/state/Battle.ts
+++ src/shared/deckforge/state/Battle.ts
@@ -1,10 +1,10 @@
-import type { BattleTeam } from "./BattleTeam";
 import type { Generics } from "./Generics";
 import type { CardPile } from "./CardPile";
+import type { PlayerId } from "./Player";
 
 export interface Battle<G extends Generics> {
   turn: number;
-  teams: BattleTeam<G>[];
   cardPiles: CardPile<G, G["battleCardPiles"]>;
+  winningPlayerId?: PlayerId;
   props: G["battleProps"];
 }
