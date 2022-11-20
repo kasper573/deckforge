b3a946d311acbbcd2e8ccddbae95cb206c0057d5
ksandin <kasper.sandin@gmail.com>
2022-11-20 01:15:24 +0100

    Add e2e test case for deck sizes of 2

--- src/shared/deckforge/e2e.test.ts
+++ src/shared/deckforge/e2e.test.ts
@@ -56,3 +56,63 @@ it("1v1: can play a one card deck and win the game", () => {
   battle = runtime.state.battles.get(battleId)!;
   expect(battle?.winner).toBe(player1.id);
 });
+
+it("1v1: can play a two card deck and win the game", () => {
+  function mockCard(damage: number): Card {
+    return {
+      id: createId(),
+      effects: {
+        playCard: [
+          (state, ids) => {
+            const target = state.players.get(ids.targetId);
+            if (target) {
+              target.health -= damage;
+            }
+          },
+        ],
+      },
+    };
+  }
+
+  const cards = new Map(
+    [mockCard(1), mockCard(-1)].map((card) => [card.id, card])
+  );
+  const deck: Deck = { id: createId(), cards: Array.from(cards.keys()) };
+  const player1: Player = { id: createId(), deck: deck.id, health: 3 };
+  const player2: Player = { id: createId(), deck: deck.id, health: 3 };
+
+  const runtime = createRuntime({
+    decks: new Map([[deck.id, deck]]),
+    cards,
+    battles: new Map(),
+    players: new Map([
+      [player1.id, player1],
+      [player2.id, player2],
+    ]),
+  });
+
+  runtime.events.startBattle({
+    member1: player1.id,
+    member2: player2.id,
+  });
+
+  const battleId = Array.from(runtime.state.battles.values())[0]?.id!;
+
+  runtime.events.drawCard({
+    playerId: player1.id,
+    battleId: battleId,
+  });
+
+  let battle = runtime.state.battles.get(battleId)!;
+  runtime.events.playCard({
+    battleId,
+    playerId: player1.id,
+    targetId: player2.id,
+    cardId: battle.member1.cards.hand[0]!,
+  });
+
+  runtime.events.endTurn(battleId);
+
+  battle = runtime.state.battles.get(battleId)!;
+  expect(battle?.winner).toBe(player1.id);
+});
