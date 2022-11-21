3dad358c7578f5e29a91e7711ff5fd659c3939d5
ksandin <kasper.sandin@gmail.com>
2022-11-21 01:52:44 +0100

    Remove next/head from root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Head from "next/head";
 import { signIn, signOut, useSession } from "next-auth/react";
 import { trpc } from "./trpcClient";
 import styles from "./page.module.css";
@@ -9,24 +8,17 @@ export default function HomePage() {
   const hello = trpc.example.hello.useQuery({ text: "from tRPC" });
 
   return (
-    <>
-      <Head>
-        <title>Deck Forge</title>
-        <meta name="description" content="Deck Builder" />
-        <link rel="icon" href="/public/favicon.ico" />
-      </Head>
-      <div className={styles.containerOuter}>
-        <div className={styles.containerInner}>
-          <h1 className={styles.title}>
-            Deck <span className={styles.titlePink}>Forge</span>
-          </h1>
-          <div className={styles.helloFrom}>
-            {hello.data ? <p>{hello.data.greeting}</p> : <p>Loading...</p>}
-          </div>
-          <AuthShowcase />
+    <div className={styles.containerOuter}>
+      <div className={styles.containerInner}>
+        <h1 className={styles.title}>
+          Deck <span className={styles.titlePink}>Forge</span>
+        </h1>
+        <div className={styles.helloFrom}>
+          {hello.data ? <p>{hello.data.greeting}</p> : <p>Loading...</p>}
         </div>
+        <AuthShowcase />
       </div>
-    </>
+    </div>
   );
 }
 
