e058c58f9c9137591eacda9d2bab20cb7ee2318e
ksandin <kasper.sandin@gmail.com>
2022-11-22 01:37:49 +0100

    Stub card list page

--- src/pages/build/[gameId]/deck/[deckId]/index.tsx
+++ src/pages/build/[gameId]/deck/[deckId]/index.tsx
@@ -1,3 +1,48 @@
+import Paper from "@mui/material/Paper";
+import List from "@mui/material/List";
+import Button from "@mui/material/Button";
+import ListItem from "@mui/material/ListItem";
+import IconButton from "@mui/material/IconButton";
+import ListItemText from "@mui/material/ListItemText";
+import Link from "../../../../../components/Link";
+import { Delete, Edit } from "../../../../../components/icons";
+import { Header } from "../../../../../components/Header";
+
 export default function DeckEditPage() {
-  return <>DeckEditPage</>;
+  return (
+    <>
+      <Header>CardEditPage</Header>
+      <Paper sx={{ mb: 3 }}>
+        <List dense>
+          <CardListItem />
+          <CardListItem />
+          <CardListItem />
+        </List>
+      </Paper>
+      <Button variant="contained">Create new card</Button>
+    </>
+  );
+}
+
+export function CardListItem() {
+  return (
+    <ListItem
+      secondaryAction={
+        <>
+          <IconButton
+            component={Link}
+            href="/build/gameId/deck/deckId/cardId"
+            aria-label="edit"
+          >
+            <Edit />
+          </IconButton>
+          <IconButton edge="end" aria-label="delete">
+            <Delete />
+          </IconButton>
+        </>
+      }
+    >
+      <ListItemText primary="Card name" />
+    </ListItem>
+  );
 }
