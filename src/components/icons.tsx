9d07c8abfb5c8dd01d4067a626138f73d65a9f5c
ksandin <kasper.sandin@gmail.com>
2022-11-22 01:03:26 +0100

    Stub game list

---
+++ src/components/icons.tsx
@@ -0,0 +1,5 @@
+import Delete from "@mui/icons-material/Delete";
+import Edit from "@mui/icons-material/Edit";
+import Play from "@mui/icons-material/PlayArrow";
+
+export { Delete, Edit, Play };
--- src/pages/build/index.tsx
+++ src/pages/build/index.tsx
@@ -1,3 +1,50 @@
+import ListItem from "@mui/material/ListItem";
+import IconButton from "@mui/material/IconButton";
+import ListItemText from "@mui/material/ListItemText";
+import Paper from "@mui/material/Paper";
+import List from "@mui/material/List";
+import { Delete, Edit, Play } from "../../components/icons";
+import Link from "../../components/Link";
+
 export default function GameEditorListPage() {
-  return <>GameEditorListPage</>;
+  return (
+    <>
+      GameEditorListPage
+      <GameList />
+    </>
+  );
+}
+
+export function GameListItem() {
+  return (
+    <ListItem
+      secondaryAction={
+        <>
+          <IconButton component={Link} href="/play/foo" aria-label="play">
+            <Play />
+          </IconButton>
+          <IconButton component={Link} href="/build/gameId" aria-label="edit">
+            <Edit />
+          </IconButton>
+          <IconButton edge="end" aria-label="delete">
+            <Delete />
+          </IconButton>
+        </>
+      }
+    >
+      <ListItemText primary="Game name" />
+    </ListItem>
+  );
+}
+
+export function GameList() {
+  return (
+    <Paper>
+      <List dense>
+        <GameListItem />
+        <GameListItem />
+        <GameListItem />
+      </List>
+    </Paper>
+  );
 }
