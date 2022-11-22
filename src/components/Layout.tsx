7072109a9ab8f0708492779b093e8537069f14c0
ksandin <kasper.sandin@gmail.com>
2022-11-22 01:18:40 +0100

    Add header and minor stylized layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,23 @@
 import { styled } from "@mui/system";
+import type { ReactNode } from "react";
+import MuiContainer from "@mui/material/Container";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Link from "./Link";
 
-export const Layout = styled("div")`
-  background: ${({ theme }) => theme.palette.background.default};
+export function Layout({ children }: { children: ReactNode }) {
+  return (
+    <>
+      <AppBar>
+        <Toolbar>
+          <Link href="/">Deck Forge</Link>
+        </Toolbar>
+      </AppBar>
+      <Toolbar />
+      <Container>{children}</Container>
+    </>
+  );
+}
+const Container = styled(MuiContainer)`
+  padding-top: ${({ theme }) => theme.spacing(2)};
 `;
