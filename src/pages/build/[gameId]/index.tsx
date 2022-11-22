6012866c80f679a1eddea1bc08c093fc54fb31dc
ksandin <kasper.sandin@gmail.com>
2022-11-22 01:26:48 +0100

    Stub game edit page

--- src/pages/build/[gameId]/index.tsx
+++ src/pages/build/[gameId]/index.tsx
@@ -1,3 +1,30 @@
+import Paper from "@mui/material/Paper";
+import List from "@mui/material/List";
+import ListItemText from "@mui/material/ListItemText";
+import ListItemButton from "@mui/material/ListItemButton";
+import { Header } from "../../../components/Header";
+import Link from "../../../components/Link";
+
 export default function GameEditPage() {
-  return <>GameEditPage</>;
+  return (
+    <>
+      <Header>GameEditPage</Header>
+      <Paper sx={{ mb: 3 }}>
+        <List dense>
+          <ListItemButton component={Link} href="/build/gameId/deck">
+            <ListItemText primary="Decks" />
+          </ListItemButton>
+          <ListItemButton component={Link} href="/build/gameId/entity">
+            <ListItemText primary="Entities" />
+          </ListItemButton>
+          <ListItemButton component={Link} href="/build/gameId/action">
+            <ListItemText primary="Actions" />
+          </ListItemButton>
+          <ListItemButton component={Link} href="/build/gameId/reaction">
+            <ListItemText primary="Reactions" />
+          </ListItemButton>
+        </List>
+      </Paper>
+    </>
+  );
 }
