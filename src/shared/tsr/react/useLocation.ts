e58e01afd6c94f89918437d7461b3cd6188a8564
ksandin <kasper.sandin@gmail.com>
2022-11-27 10:25:24 +0100

    Get rid of TSR

--- src/shared/tsr/react/useLocation.ts
+++
@@ -1,7 +0,0 @@
-import { useContext } from "react";
-import { RouterContext } from "./RouterContext";
-
-export function useLocation() {
-  const { location } = useContext(RouterContext);
-  return location;
-}
