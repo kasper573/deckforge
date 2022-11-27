3ee31fb8ba1aaf0158c5146de2f7844500bb0821
ksandin <kasper.sandin@gmail.com>
2022-11-27 06:30:28 +0100

    e2e test for signing out

--- cypress/e2e/auth.spec.ts
+++ cypress/e2e/auth.spec.ts
@@ -1,9 +1,17 @@
+import { assertSignedIn, signIn, signOut } from "../support/actions";
+
+beforeEach(() => cy.visit("/"));
+
 describe("auth", () => {
   it("becomes signed in after clicking sign in", () => {
-    cy.visit("/");
-    cy.findByRole("button", { name: /show user menu/i }).click();
-    cy.findByRole(/button|menuitem/, { name: /sign in/i }).click();
-    // Sign in flow is handled outside the app. Assumes it's completed (i.e. we've enabled a fake)
-    cy.findByTestId("online-indicator").should("exist");
+    signIn();
+    assertSignedIn();
+  });
+
+  it("becomes signed out after clicking sign out", () => {
+    signIn();
+    assertSignedIn();
+    signOut();
+    assertSignedIn(false);
   });
 });
