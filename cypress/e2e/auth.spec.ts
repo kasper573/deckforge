7bba3fe842d81fbb64c585ac47af3f68e9d99972
ksandin <kasper.sandin@gmail.com>
2022-11-27 05:04:46 +0100

    Add explanation

--- cypress/e2e/auth.spec.ts
+++ cypress/e2e/auth.spec.ts
@@ -3,6 +3,7 @@ describe("auth", () => {
     cy.visit("/");
     cy.findByRole("button", { name: "show user menu" }).click();
     cy.findByRole("button", { name: "sign in" }).click();
+    // Sign in flow is handled outside the app. Assumes it's completed (i.e. we've enabled a fake)
     cy.findByTestId("online-indicator").should("exist");
   });
 });
