d41dd24044659a4f38e83ca87cca3a1e43c6e3bd
ksandin <kasper.sandin@gmail.com>
2022-11-29 08:55:17 +0100

    Header should be a div to allow nested paragraphs

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,6 +1,6 @@
-import type { ComponentProps } from "react";
+import type { HTMLAttributes } from "react";
 import Typography from "@mui/material/Typography";
 
-export function Header(props: ComponentProps<typeof Typography>) {
-  return <Typography variant="h5" paragraph {...props} />;
+export function Header(props: HTMLAttributes<HTMLDivElement>) {
+  return <Typography component="div" variant="h5" paragraph {...props} />;
 }
