ae6d3a05031c5aa30de0772c2b8f5507eabd2189
ksandin <kasper.sandin@gmail.com>
2022-11-29 15:44:10 +0100

    Only expose user facing errors in production

--- src/api/trpc.ts
+++ src/api/trpc.ts
@@ -1,14 +1,31 @@
+import type { DefaultErrorShape } from "@trpc/server";
 import { initTRPC } from "@trpc/server";
 import superjson from "superjson";
 import type { PrismaClient } from "@prisma/client";
 import type { AuthContext } from "./services/auth/types";
+import { env } from "./env";
+import { UserFacingError } from "./utils/UserFacingError";
 
 export const t = initTRPC.context<Context>().create({
   transformer: superjson,
-  errorFormatter({ shape }) {
-    return shape;
+  errorFormatter({ shape, error }) {
+    return env.exposeInternalErrors || error instanceof UserFacingError
+      ? shape
+      : stripInternalError(shape);
   },
 });
+
+function stripInternalError(shape: DefaultErrorShape): DefaultErrorShape {
+  return {
+    ...shape,
+    message: "Internal Server Error",
+    data: {
+      ...shape.data,
+      path: undefined,
+      stack: undefined,
+    },
+  };
+}
 
 export type Context = {
   auth?: AuthContext;
---
+++ src/api/utils/UserFacingError.ts
@@ -0,0 +1,6 @@
+export class UserFacingError extends Error {
+  constructor(message: string) {
+    super(message);
+    Object.setPrototypeOf(this, new.target.prototype);
+  }
+}
