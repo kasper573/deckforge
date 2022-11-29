a3e19e18d1f4b85a1d45e837edfc3fcc6377f84c
ksandin <kasper.sandin@gmail.com>
2022-11-29 06:25:32 +0100

    Invalidate all queries on any mutation

--- src/app/trpc.ts
+++ src/app/trpc.ts
@@ -6,7 +6,19 @@ import superjson from "superjson";
 import type { ApiRouter } from "../api/router";
 import { env } from "./env";
 
-export const trpc = createTRPCReact<ApiRouter>();
+export const trpc = createTRPCReact<ApiRouter>({
+  // Invalidate any and all queries whenever a mutation is performed
+  // This is to emulate the automatic invalidation that rtk-query would provide (which is what we would want).
+  // But tRPC has no rtk-query bindings, so instead we have to make to with this solution for react-query.
+  unstable_overrides: {
+    useMutation: {
+      async onSuccess(opts) {
+        await opts.originalFn();
+        await opts.queryClient.invalidateQueries();
+      },
+    },
+  },
+});
 
 export const queryClient = new QueryClient();
 
