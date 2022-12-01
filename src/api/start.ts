3867d919049e18398f2136ee0629d7413f248182
ksandin <kasper.sandin@gmail.com>
2022-12-01 05:55:39 +0100

    Clarify intent of start.ts

--- src/api/start.ts
+++ src/api/start.ts
@@ -4,6 +4,8 @@ import cors from "cors";
 import { createServer } from "./server";
 import { env } from "./env";
 
+// Used for development and testing.
+// In production, this entry point is not used.
 const api = express();
 api.use(cors());
 api.use(morgan("dev"));
