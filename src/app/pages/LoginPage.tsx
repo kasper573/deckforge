122fde229a3b9a3d13deed6e1f975c5c077ee551
ksandin <kasper.sandin@gmail.com>
2022-12-02 04:19:21 +0100

    Improve login page

--- src/app/pages/LoginPage.tsx
+++ src/app/pages/LoginPage.tsx
@@ -1,43 +1,51 @@
 import TextField from "@mui/material/TextField";
 import type { FormEvent } from "react";
-import { useRef, useState } from "react";
+import { useRef } from "react";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
 import { Page } from "../layout/Page";
-import { Header } from "../layout/Header";
 import { Center } from "../components/Center";
 import { useAuth } from "../features/auth/store";
+import { Link } from "../components/Link";
+import { router } from "../router";
 
 export default function LoginPage() {
   const { login } = useAuth();
-  const [errorMessage, setErrorMessage] = useState<string>();
   const usernameRef = useRef<HTMLInputElement>(null);
   const passwordRef = useRef<HTMLInputElement>(null);
 
   async function submit(e: FormEvent) {
     e.preventDefault();
 
-    setErrorMessage(undefined);
-    const result = await login({
+    login.mutateAsync({
       username: usernameRef.current?.value ?? "",
       password: passwordRef.current?.value ?? "",
     });
-    if (!result.success) {
-      setErrorMessage(result.message);
-    }
   }
 
   return (
     <Page>
-      <Header>Login</Header>
       <Center>
         <form onSubmit={submit}>
-          <TextField label="Username" ref={usernameRef} />
-          <TextField label="Password" ref={passwordRef} />
-          {errorMessage && (
-            <Typography color="error">{errorMessage}</Typography>
-          )}
-          <Button type="submit">Login</Button>
+          <Stack direction="column" spacing={2} sx={{ width: 350 }}>
+            <TextField size="small" label="Username" ref={usernameRef} />
+            <TextField size="small" label="Password" ref={passwordRef} />
+            <Stack direction="row" spacing={2} alignItems="center">
+              <Typography color="error" sx={{ flex: 1 }}>
+                {login.error?.message}
+              </Typography>
+              <div>
+                <Button type="submit" variant="contained">
+                  Login
+                </Button>
+              </div>
+            </Stack>
+            <Box sx={{ textAlign: "right" }}>
+              <Link to={router.user().register()}>Register an account</Link>
+            </Box>
+          </Stack>
         </form>
       </Center>
     </Page>
