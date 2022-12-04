8c360f0437e0f2771c66af241c831c1947c6e984
ksandin <kasper.sandin@gmail.com>
2022-12-04 12:41:05 +0100

    Normalize and support intersections in zodTypeAtPath

--- src/lib/zod-extensions/zodTypeAtPath.ts
+++ src/lib/zod-extensions/zodTypeAtPath.ts
@@ -1,15 +1,47 @@
 import type { ZodType } from "zod";
-import { ZodObject } from "zod";
+import {
+  ZodDefault,
+  ZodEffects,
+  ZodIntersection,
+  ZodNullable,
+  ZodObject,
+  ZodOptional,
+} from "zod";
 
-export function zodTypeAtPath(schema: ZodType, path: string): ZodType {
-  const parts = path.split(".");
-  let type: ZodType = schema;
-  for (const part of parts) {
-    if (type instanceof ZodObject) {
-      type = type.shape[part];
-    } else {
-      throw new Error(`Expected ZodObject, got ${type.constructor.name}`);
+export function zodTypeAtPath(
+  type: ZodType,
+  path: string
+): ZodType | undefined {
+  type = normalizeType(type);
+
+  if (type instanceof ZodObject) {
+    const [first, ...rest] = path.split(".");
+    type = type.shape[first];
+    if (!rest.length) {
+      return type;
     }
+    return zodTypeAtPath(type, rest.join("."));
+  }
+
+  if (type instanceof ZodIntersection) {
+    return (
+      zodTypeAtPath(type._def.left, path) ||
+      zodTypeAtPath(type._def.right, path)
+    );
+  }
+
+  // Should never happen (if it happens there's a flaw in the implementation)
+  throw new Error(`Unsupported zod type: ${type.constructor.name}`);
+}
+
+function normalizeType(type: ZodType) {
+  while (
+    type instanceof ZodEffects ||
+    type instanceof ZodOptional ||
+    type instanceof ZodNullable ||
+    type instanceof ZodDefault
+  ) {
+    type = type instanceof ZodEffects ? type.innerType() : type._def.innerType;
   }
   return type;
 }
