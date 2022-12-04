50ce0a4bcd2062dd880a29919cd22d157a12047c
ksandin <kasper.sandin@gmail.com>
2022-12-04 12:55:09 +0100

    Separate zodNormalize to its own module

--- src/lib/zod-extensions/zodTypeAtPath.ts
+++ src/lib/zod-extensions/zodTypeAtPath.ts
@@ -1,12 +1,6 @@
 import type { ZodType } from "zod";
-import {
-  ZodDefault,
-  ZodEffects,
-  ZodIntersection,
-  ZodNullable,
-  ZodObject,
-  ZodOptional,
-} from "zod";
+import { ZodIntersection, ZodObject } from "zod";
+import { normalizeType } from "./zodNormalize";
 
 export function zodTypeAtPath(
   type: ZodType,
@@ -32,16 +26,4 @@ export function zodTypeAtPath(
 
   // Should never happen (if it happens there's a flaw in the implementation)
   throw new Error(`Unsupported zod type: ${type.constructor.name}`);
-}
-
-function normalizeType(type: ZodType) {
-  while (
-    type instanceof ZodEffects ||
-    type instanceof ZodOptional ||
-    type instanceof ZodNullable ||
-    type instanceof ZodDefault
-  ) {
-    type = type instanceof ZodEffects ? type.innerType() : type._def.innerType;
-  }
-  return type;
 }
