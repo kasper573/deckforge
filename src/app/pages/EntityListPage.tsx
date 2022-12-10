691ff52d5440cebd39ff53da4b187af17e949363
ksandin <kasper.sandin@gmail.com>
2022-12-10 19:00:02 +0100

    Convert entity list page to local state

--- src/app/pages/EntityListPage.tsx
+++ src/app/pages/EntityListPage.tsx
@@ -1,32 +1,36 @@
 import Paper from "@mui/material/Paper";
 import List from "@mui/material/List";
 import ListItemText from "@mui/material/ListItemText";
-import { useRouteParams } from "react-typesafe-routes";
 import { Header } from "../components/Header";
 import { Page } from "../layout/Page";
 import { LinkListItem } from "../components/Link";
 import { router } from "../router";
-import { trpc } from "../trpc";
-import type { Entity } from "../../api/services/entity/types";
+import { useSelector } from "../store";
+import type { EntityId } from "../../api/services/game/types";
 
 export default function EntityListPage() {
-  const { gameId } = useRouteParams(router.build().game);
-  const { data: entities } = trpc.entity.listEntities.useQuery({ gameId });
+  const { gameId } = useSelector((state) => state.game);
   return (
     <Page>
       <Header>Game: {gameId}. Entity List</Header>
       <Paper sx={{ mb: 3 }}>
         <List dense>
-          {entities?.map((entity) => (
-            <EntityListItem key={entity.entityId} {...entity} />
-          ))}
+          <EntityListItem entityId="card" name="Card" />
+          <EntityListItem entityId="player" name="Player" />
         </List>
       </Paper>
     </Page>
   );
 }
 
-export function EntityListItem({ gameId, entityId, name }: Entity) {
+export function EntityListItem({
+  entityId,
+  name,
+}: {
+  entityId: EntityId;
+  name: string;
+}) {
+  const { gameId } = useSelector((state) => state.game);
   return (
     <LinkListItem
       to={router.build().game({ gameId }).entity().edit({ entityId })}
