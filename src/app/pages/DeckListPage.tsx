9e56ce0e4a3d88af6a3d3eea97ee9ea550b2ba92
ksandin <kasper.sandin@gmail.com>
2022-12-10 19:09:36 +0100

    Convert deck list page to local state

--- src/app/pages/DeckListPage.tsx
+++ src/app/pages/DeckListPage.tsx
@@ -5,7 +5,6 @@ import ListItem from "@mui/material/ListItem";
 import IconButton from "@mui/material/IconButton";
 import ListItemText from "@mui/material/ListItemText";
 import { useRouteParams } from "react-typesafe-routes";
-import type { Deck } from "@prisma/client";
 import Typography from "@mui/material/Typography";
 import { LinkIconButton } from "../components/Link";
 import { Delete, Edit } from "../components/icons";
@@ -16,18 +15,16 @@ import { trpc } from "../trpc";
 import { useModal } from "../../lib/useModal";
 import { PromptDialog } from "../dialogs/PromptDialog";
 import { ConfirmDialog } from "../dialogs/ConfirmDialog";
-import { useToastProcedure } from "../hooks/useToastProcedure";
+import { useSelector } from "../store";
+import { editorActions, selectors } from "../features/editor/editorState";
+import { useActions } from "../../lib/useActions";
+import type { Deck } from "../../api/services/game/types";
 
 export default function DeckListPage() {
   const { gameId } = useRouteParams(router.build().game);
   const { data: game } = trpc.game.read.useQuery(gameId);
-  const { data: decks } = trpc.deck.list.useQuery({
-    filter: { gameId },
-    offset: 0,
-    limit: 10,
-  });
-
-  const createDeck = useToastProcedure(trpc.deck.create);
+  const decks = useSelector(selectors.decks);
+  const { createDeck } = useActions(editorActions);
   const prompt = useModal(PromptDialog);
 
   return (
@@ -35,10 +32,10 @@ export default function DeckListPage() {
       <Header>Game: {game?.name}. Deck List</Header>
       <Paper sx={{ mb: 3 }}>
         <List dense aria-label="Decks">
-          {decks?.entities.map((deck) => (
+          {decks.map((deck) => (
             <DeckListItem key={deck.deckId} {...deck} />
           ))}
-          {decks?.total === 0 && (
+          {decks.length === 0 && (
             <Typography align="center">
               {"This game doesn't contain any decks yet."}
             </Typography>
@@ -52,7 +49,7 @@ export default function DeckListPage() {
             title: "Create new deck",
             fieldProps: { label: "Deck name" },
           }).then((name) => {
-            name && createDeck.mutate({ gameId, name });
+            name && createDeck({ name });
           })
         }
       >
@@ -62,9 +59,10 @@ export default function DeckListPage() {
   );
 }
 
-export function DeckListItem({ gameId, deckId, name }: Deck) {
+export function DeckListItem({ deckId, name }: Deck) {
+  const { gameId } = useSelector(selectors.game);
   const confirm = useModal(ConfirmDialog);
-  const deleteDeck = useToastProcedure(trpc.deck.delete);
+  const { deleteDeck } = useActions(editorActions);
 
   async function confirmDelete() {
     const shouldDelete = await confirm({
@@ -72,7 +70,7 @@ export function DeckListItem({ gameId, deckId, name }: Deck) {
       content: `Are you sure you want to delete "${name}". This action cannot be reversed.`,
     });
     if (shouldDelete) {
-      deleteDeck.mutate(deckId);
+      deleteDeck(deckId);
     }
   }
 
