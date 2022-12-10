613e4209ff26acd26e0a793b44a8fb1769f25fd0
ksandin <kasper.sandin@gmail.com>
2022-12-10 12:41:18 +0100

    Simplify props type

--- src/app/components/CodeEditor.tsx
+++ src/app/components/CodeEditor.tsx
@@ -1,18 +1,12 @@
-import type { ComponentProps } from "react";
 import Editor from "@monaco-editor/react";
 import { useDebouncedControl } from "../hooks/useDebouncedControl";
 
-export interface CodeEditorProps
-  extends Omit<ComponentProps<typeof Editor>, "onChange" | "value"> {
+export interface CodeEditorProps {
   value?: string;
   onChange: (value: string) => void;
 }
 
-export function CodeEditor({
-  value = "",
-  onChange,
-  ...props
-}: CodeEditorProps) {
+export function CodeEditor({ value = "", onChange }: CodeEditorProps) {
   const control = useDebouncedControl({ value, onChange });
   return (
     <Editor
@@ -20,7 +14,6 @@ export function CodeEditor({
       theme="vs-dark"
       value={control.value}
       onChange={(newValue = "") => control.setValue(newValue)}
-      {...props}
     />
   );
 }
