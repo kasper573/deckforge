080d2c4d54754a413ff2d64241ec173a3c6071dc
ksandin <kasper.sandin@gmail.com>
2022-12-12 22:02:07 +0100

    Update design of editor app bar

--- src/app/features/editor/components/EditorAppBar.tsx
+++ src/app/features/editor/components/EditorAppBar.tsx
@@ -1,6 +1,8 @@
 import Stack from "@mui/material/Stack";
 import IconButton from "@mui/material/IconButton";
 import Tooltip from "@mui/material/Tooltip";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 import { selectors } from "../selectors";
 import { useSelector } from "../../../store";
 import { useActions } from "../../../../lib/useActions";
@@ -8,7 +10,6 @@ import { editorActions } from "../actions";
 import { Edit, ExitToApp } from "../../../components/icons";
 import { useModal } from "../../../../lib/useModal";
 import { PromptDialog } from "../../../dialogs/PromptDialog";
-import { Header } from "../../layout/Header";
 import { LinkIconButton } from "../../../components/Link";
 import { router } from "../../../router";
 import { AppBar } from "../../layout/AppBar";
@@ -30,7 +31,7 @@ export default function EditorAppBar() {
 
   return (
     <AppBar container={false}>
-      <Stack direction="row" spacing={2} alignItems="center">
+      <Stack direction="row" sx={{ display: "flex" }}>
         <Tooltip title="Leave editor">
           <div>
             <LinkIconButton edge="start" to={router.build()}>
@@ -38,14 +39,23 @@ export default function EditorAppBar() {
             </LinkIconButton>
           </div>
         </Tooltip>
-        <Header sx={{ m: 0 }}>{game.name}</Header>
-        <Tooltip title="Rename">
-          <div>
-            <IconButton onClick={promptRename}>
-              <Edit />
-            </IconButton>
-          </div>
-        </Tooltip>
+        <Box sx={{ flex: 1, display: "flex", alignItems: "space-around" }}>
+          <Stack
+            direction="row"
+            spacing={2}
+            alignItems="center"
+            sx={{ margin: "auto" }}
+          >
+            <Typography>{game.name}</Typography>
+            <Tooltip title="Rename">
+              <div>
+                <IconButton onClick={promptRename}>
+                  <Edit />
+                </IconButton>
+              </div>
+            </Tooltip>
+          </Stack>
+        </Box>
       </Stack>
     </AppBar>
   );
