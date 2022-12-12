7f6873670a2938c79c79e8bd9ad1f89e6d50fbbb
ksandin <kasper.sandin@gmail.com>
2022-12-12 21:27:18 +0100

    Refactor

---
+++ src/app/features/layout/UserMenu.tsx
@@ -0,0 +1,55 @@
+import IconButton from "@mui/material/IconButton";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import Divider from "@mui/material/Divider";
+import MenuItem from "@mui/material/MenuItem";
+import { useAuth } from "../auth/store";
+import { MenuFor } from "../../components/MenuFor";
+import { OnlineBadge } from "../../components/OnlineBadge";
+import { Auth } from "../auth/Auth";
+import { LinkMenuItem } from "../../components/Link";
+import { router } from "../../router";
+
+export function UserMenu() {
+  const { user, logout } = useAuth();
+  return (
+    <MenuFor
+      MenuListProps={{ "aria-label": "User menu" }}
+      trigger={({ open }) => (
+        <IconButton aria-label="show user menu" sx={{ ml: 1 }} onClick={open}>
+          {user ? (
+            <OnlineBadge data-testid="online-indicator">
+              <AccountCircle />
+            </OnlineBadge>
+          ) : (
+            <AccountCircle />
+          )}
+        </IconButton>
+      )}
+    >
+      <Auth exact="Guest">
+        <LinkMenuItem to={router.user().login()}>Sign in</LinkMenuItem>
+        <LinkMenuItem to={router.user().register()}>Register</LinkMenuItem>
+      </Auth>
+      <Auth>
+        <ListItem sx={{ pt: 0 }}>
+          <ListItemText
+            primaryTypographyProps={{ noWrap: true }}
+            primary={
+              <>
+                Signed in as <strong>{user?.name}</strong>
+              </>
+            }
+          />
+        </ListItem>
+        <Divider sx={{ mb: 1 }} />
+        <LinkMenuItem to={router.build()}>Your games</LinkMenuItem>
+        <LinkMenuItem to={router.user().profile()}>
+          Account settings
+        </LinkMenuItem>
+        <MenuItem onClick={() => logout()}>Sign out</MenuItem>
+      </Auth>
+    </MenuFor>
+  );
+}
