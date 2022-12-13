4fd9d380e18f27a28ed05b6832783f083fcaa5a0
ksandin <kasper.sandin@gmail.com>
2022-12-13 18:42:26 +0100

    Vertical padding on project panel content

--- src/app/features/editor/panels/ProjectPanel.tsx
+++ src/app/features/editor/panels/ProjectPanel.tsx
@@ -4,6 +4,7 @@ import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
 import type { MosaicBranch } from "react-mosaic-component";
 import Stack from "@mui/material/Stack";
+import { styled } from "@mui/material/styles";
 import { DeckTree } from "../components/DeckTree";
 import { EventTree } from "../components/EventTree";
 import { PropertyTree } from "../components/PropertyTree";
@@ -18,7 +19,7 @@ const tabs = [
 export function ProjectPanel({ path }: { path: MosaicBranch[] }) {
   const [tabIndex, setTabIndex] = useState(0);
   return (
-    <Panel sx={{ py: 2 }} title="Project" path={path}>
+    <Panel title="Project" path={path}>
       <Stack direction="column" sx={{ flex: 1 }}>
         <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
           <Tabs
@@ -31,8 +32,13 @@ export function ProjectPanel({ path }: { path: MosaicBranch[] }) {
             ))}
           </Tabs>
         </Box>
-        {tabs[tabIndex].content}
+        <ProjectPanelContent>{tabs[tabIndex].content}</ProjectPanelContent>
       </Stack>
     </Panel>
   );
 }
+
+const ProjectPanelContent = styled("div")`
+  padding-top: ${({ theme }) => theme.spacing(2)};
+  padding-bottom: ${({ theme }) => theme.spacing(2)};
+`;
