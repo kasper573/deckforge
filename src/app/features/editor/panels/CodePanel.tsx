cff9531a34521ec6fdfb9f2beb119a7ebb5ab2e7
ksandin <kasper.sandin@gmail.com>
2022-12-14 18:02:50 +0100

    Reuse same monaco editor instance when swapping between object types

--- src/app/features/editor/panels/CodePanel.tsx
+++ src/app/features/editor/panels/CodePanel.tsx
@@ -1,77 +1,58 @@
 import { useSelector } from "../../../store";
 import { selectors } from "../selectors";
-import type {
-  ActionId,
-  CardId,
-  ReactionId,
-} from "../../../../api/services/game/types";
+
 import { useActions } from "../../../../lib/useActions";
 import { editorActions } from "../actions";
 import { CodeEditor } from "../../../components/CodeEditor";
 import { Panel } from "../components/Panel";
 import { PanelEmptyState } from "../components/PanelEmptyState";
+import type { EditorState } from "../types";
 import type { PanelProps } from "./definition";
 
 export function CodePanel(props: PanelProps) {
-  return (
-    <Panel {...props}>
-      <ObjectCodeEditor />
-    </Panel>
+  const { selector, update, error } = useEditorProps();
+  const object = useSelector(selector);
+
+  const content = error ? (
+    <PanelEmptyState>{error}</PanelEmptyState>
+  ) : (
+    <CodeEditor value={object?.code} onChange={update} />
   );
+
+  return <Panel {...props}>{content}</Panel>;
 }
 
-export function ObjectCodeEditor() {
+function useEditorProps(): {
+  selector: (state: EditorState) => undefined | { code: string };
+  update: (code: string) => void;
+  error?: string;
+} {
   const id = useSelector(selectors.selectedObject);
-  if (!id) {
-    return <PanelEmptyState>Select an object to edit its code</PanelEmptyState>;
-  }
-  switch (id.type) {
+  const actions = useActions(editorActions);
+
+  switch (id?.type) {
     case "card":
-      return <CardCodeEditor cardId={id.cardId} />;
+      return {
+        selector: selectors.card(id.cardId),
+        update: (code) => actions.updateCard({ ...id, code }),
+      };
     case "action":
-      return <ActionCodeEditor actionId={id.actionId} />;
+      return {
+        selector: selectors.action(id.actionId),
+        update: (code) => actions.updateAction({ ...id, code }),
+      };
     case "reaction":
-      return <ReactionCodeEditor reactionId={id.reactionId} />;
+      return {
+        selector: selectors.reaction(id.reactionId),
+        update: (code) => actions.updateReaction({ ...id, code }),
+      };
   }
-  return (
-    <PanelEmptyState>
-      Objects of type {`"${id.type}"`} has no code that can be edited
-    </PanelEmptyState>
-  );
-}
 
-function ActionCodeEditor({ actionId }: { actionId: ActionId }) {
-  const action = useSelector(selectors.action(actionId));
-  const { updateAction } = useActions(editorActions);
-
-  return (
-    <CodeEditor
-      value={action?.code}
-      onChange={(code) => updateAction({ actionId, code })}
-    />
-  );
-}
-
-function ReactionCodeEditor({ reactionId }: { reactionId: ReactionId }) {
-  const reaction = useSelector(selectors.reaction(reactionId));
-  const { updateReaction } = useActions(editorActions);
-
-  return (
-    <CodeEditor
-      value={reaction?.code}
-      onChange={(code) => updateReaction({ reactionId, code })}
-    />
-  );
-}
-
-function CardCodeEditor({ cardId }: { cardId: CardId }) {
-  const card = useSelector(selectors.card(cardId));
-  const { updateCard } = useActions(editorActions);
-
-  return (
-    <CodeEditor
-      value={card?.code}
-      onChange={(code) => updateCard({ cardId, code })}
-    />
-  );
+  return {
+    selector: () => undefined,
+    update: () => {},
+    error: id
+      ? `Objects of type {\`"${id.type}"\`} has no code that can be edited`
+      : "Select an object to edit its code",
+  };
 }
