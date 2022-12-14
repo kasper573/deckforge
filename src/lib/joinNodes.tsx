521f6ea8e5403dea30bb6d66b1057e87143a1ab2
ksandin <kasper.sandin@gmail.com>
2022-12-14 19:40:00 +0100

    Avoid react keys in joinNodes

--- src/lib/joinNodes.tsx
+++ src/lib/joinNodes.tsx
@@ -1,11 +1,14 @@
 import type { ReactNode } from "react";
 
 export function joinNodes(nodes: ReactNode[], separator: ReactNode) {
-  return nodes.reduce((result: ReactNode[], node, index) => {
-    if (index > 0) {
-      result.push(separator);
-    }
-    result.push(node);
-    return result;
-  }, []);
+  return nodes.reduce(
+    (acc: ReactNode, node, index) => (
+      <>
+        {acc}
+        {index > 0 ? separator : undefined}
+        {node}
+      </>
+    ),
+    <></>
+  );
 }
