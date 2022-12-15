46a0697de1cae1a55aab905a103e1bfb5d18c7d6
ksandin <kasper.sandin@gmail.com>
2022-12-15 15:40:32 +0100

    Show pointer cursor when pile can be interacted with

--- src/app/features/runtime/CardPile.tsx
+++ src/app/features/runtime/CardPile.tsx
@@ -4,10 +4,16 @@ import { Card } from "./Card";
 export function CardPile({
   name,
   size,
+  sx,
+  onClick,
   ...props
 }: { name: string; size: number } & ComponentProps<typeof Card>) {
   return (
-    <Card {...props}>
+    <Card
+      sx={{ cursor: onClick ? "pointer" : undefined, ...sx }}
+      onClick={onClick}
+      {...props}
+    >
       {name}
       <br />
       {size}
