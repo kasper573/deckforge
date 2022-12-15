0a7ccd03bc2bbacba6605c551a7200b08969c5b8
ksandin <kasper.sandin@gmail.com>
2022-12-15 18:58:14 +0100

    Prototype implementation of effect compiler

--- src/app/features/renderer/compileGame.ts
+++ src/app/features/renderer/compileGame.ts
@@ -1,7 +1,9 @@
 import { groupBy } from "lodash";
+import { z } from "zod";
 import type { Card, Game } from "../../../api/services/game/types";
 import type { GameState } from "../runtime/Runtime";
 import { createGameRuntime } from "../runtime/Runtime";
+import type { Effects } from "../runtime/Entities";
 import { RuntimeCard, RuntimeDeck } from "../runtime/Entities";
 
 export type GameCompilerInitialState = Partial<
@@ -34,9 +36,40 @@ export function compileGame(
 function compileCard(card: Card): RuntimeCard {
   class CompiledCard extends RuntimeCard {
     constructor() {
-      super({ id: card.cardId, name: card.name });
+      super({
+        id: card.cardId,
+        name: card.name,
+      });
+      const result = compileEffects(card.code);
+      if (!result.success) {
+        throw new Error(result.error);
+      }
+      this.effects = result.effects;
     }
   }
 
   return new CompiledCard();
+}
+
+const effectType = z.function().args(z.any()).returns(z.any());
+const effectsType = z.record(z.array(effectType));
+
+function compileEffects(
+  code: string
+): { success: true; effects: Effects } | { success: false; error: string } {
+  const stringAssumedToContainObject = code.trim() || "{}";
+  const evalResult = eval(`(${stringAssumedToContainObject})`);
+  const parseResult = effectsType.safeParse(evalResult);
+  if (!parseResult.success) {
+    return { success: false, error: `${parseResult.error}` };
+  }
+  const effectsWithThisArg = Object.entries(parseResult.data).reduce(
+    (effects, [effectName, effectFns]) => ({
+      ...effects,
+      [effectName]: effectFns,
+    }),
+    {} as Effects
+  );
+
+  return { success: true, effects: effectsWithThisArg };
 }
