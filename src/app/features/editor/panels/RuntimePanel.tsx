dab3d212d7467fb8c59ed2bb4acd3b33eaa4f995
ksandin <kasper.sandin@gmail.com>
2022-12-15 14:28:58 +0100

    Catch runtime errors with error boundary in runtime panel

--- src/app/features/editor/panels/RuntimePanel.tsx
+++ src/app/features/editor/panels/RuntimePanel.tsx
@@ -2,13 +2,21 @@ import { useSelector } from "../../../store";
 import { selectors } from "../selectors";
 import { Panel } from "../components/Panel";
 import { GameRuntime } from "../../runtime/GameRuntime";
+import { ErrorBoundary } from "../../../ErrorBoundary";
+import { PanelEmptyState } from "../components/PanelEmptyState";
 import type { PanelProps } from "./definition";
 
 export function RuntimePanel(props: PanelProps) {
   const game = useSelector(selectors.game);
   return (
     <Panel {...props}>
-      {game && <GameRuntime game={game} sx={{ flex: 1 }} />}
+      <ErrorBoundary
+        fallback={({ error }) => (
+          <PanelEmptyState>{error.message}</PanelEmptyState>
+        )}
+      >
+        {game && <GameRuntime game={game} sx={{ flex: 1 }} />}
+      </ErrorBoundary>
     </Panel>
   );
 }
