3eb51159c14b59fc60425031b751c3f13dab3ff4
ksandin <kasper.sandin@gmail.com>
2022-12-22 00:43:50 +0100

    Code generation for effects

--- src/app/features/compiler/compileEditorApi.ts
+++ src/app/features/compiler/compileEditorApi.ts
@@ -1,7 +1,7 @@
 import { memoize } from "lodash";
-import type { Game, PropertyValueType } from "../../../api/services/game/types";
+import type { Game } from "../../../api/services/game/types";
 import type { CodeEditorTypeDefs } from "../../components/CodeEditor";
-import type { Property } from "../../../api/services/game/types";
+import type { Event } from "../../../api/services/game/types";
 
 export interface EditorApi {
   card: EditorObjectApi;
@@ -14,13 +14,17 @@ export interface EditorObjectApi {
 }
 
 export function compileEditorApi(game: Game): EditorApi {
-  const { properties } = game.definition;
+  const { events, properties } = game.definition;
   const playerProperties = properties.filter((p) => p.entityId === "player");
   const cardProperties = properties.filter((p) => p.entityId === "card");
   const common: CodeEditorTypeDefs = add(
     defineInterface("Player", playerProperties),
     defineInterface("Card", cardProperties),
-    defineInterface("Effects", [])
+    defineInterface(
+      "Effects",
+      eventsToEffectMembers({ stateTypeName: "State", events })
+    ),
+    defineInterface("State", [])
   );
   return {
     card: {
@@ -46,6 +50,26 @@ export function compileEditorApi(game: Game): EditorApi {
   };
 }
 
+function eventsToEffectMembers({
+  stateTypeName,
+  events,
+}: {
+  stateTypeName: string;
+  events: Event[];
+}): Member[] {
+  return events.map(({ name, inputType }) => ({
+    name,
+    type: defineEffectFunction({ stateTypeName, inputType }),
+  }));
+}
+
+function defineEffectFunction(p: { stateTypeName: string; inputType: Type }) {
+  return `(state: ${p.stateTypeName}, input: ${defineType(
+    p.inputType,
+    2
+  )}) => void`;
+}
+
 function defineFactoryType(p: { inputType: string; outputType: string }) {
   return `(definition: ${p.inputType}) => ${p.outputType}`;
 }
@@ -54,11 +78,11 @@ function defineGlobalVariable(p: { name: string; type: string }): string {
   return `declare let ${p.name}: ${p.type};`;
 }
 
-function defineInterface(interfaceName: string, properties: Property[]) {
-  return `interface ${interfaceName} ${defineObjectType(properties)}`;
+function defineInterface(interfaceName: string, members: Member[]) {
+  return `interface ${interfaceName} ${defineObjectType(members)}`;
 }
 
-function defineType(type: PropertyValueType, indentation?: number): string {
+function defineType(type: Type, indentation?: number): string {
   if (typeof type === "string") {
     return type;
   }
@@ -69,17 +93,17 @@ function defineType(type: PropertyValueType, indentation?: number): string {
   return defineObjectType(properties, indentation);
 }
 
-function defineObjectType(
-  properties: Pick<Property, "name" | "type">[],
-  indentation = 1
-) {
-  const propertyStrings = properties.map(
+function defineObjectType(members: Member[], indentation = 1) {
+  const propertyStrings = members.map(
     ({ name, type }) =>
       `${indent(indentation)}${name}: ${defineType(type, indentation + 1)}`,
     "{\n"
   );
   return `{\n${propertyStrings.join(";\n")}\n${indent(indentation - 1)}}`;
 }
+
+type Type = string | { [name: string]: Type };
+type Member = { name: string; type: Type };
 
 const indent = memoize((indentation: number) => "\t".repeat(indentation));
 
