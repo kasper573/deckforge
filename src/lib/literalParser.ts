03e6937a7aa74ab9db3feaaa751a9401d7dcea3f
ksandin <kasper.sandin@gmail.com>
2023-01-09 04:45:48 +0100

    Create optionalLiteralParser

--- src/lib/literalParser.ts
+++ src/lib/literalParser.ts
@@ -13,3 +13,17 @@ export function literalParser<Value extends string>(
     serialize: (x: Value) => x,
   };
 }
+
+export function optionalLiteralParser<Value extends string>(
+  values?: Value[]
+): ParamParser<Value | undefined> {
+  return {
+    parse: (s: string) => {
+      if (values && !values.includes(s as Value)) {
+        throw new Error(`Must be one of: ${values.join(", ")}`);
+      }
+      return s ? (s as Value) : undefined;
+    },
+    serialize: (x?: Value) => x ?? "",
+  };
+}
