e60fd7cce63741690a6fee4f2d700ef07b760592
ksandin <kasper.sandin@gmail.com>
2023-01-21 02:01:05 +0100

    Suspend game renderer

--- src/app/features/gameTypes/LazyGameRenderer.tsx
+++ src/app/features/gameTypes/LazyGameRenderer.tsx
@@ -1,13 +1,26 @@
-import type { ComponentProps } from "react";
+import type { ComponentProps, ReactNode } from "react";
+import { Suspense } from "react";
 import type { RuntimeGenerics } from "../compiler/types";
 import type { GameTypeId } from "../../../api/services/game/types";
+import { LoadingIndicator } from "../../components/LoadingIndicator";
+import { Center } from "../../components/Center";
 import type { GameRendererProps } from "./GameType";
 import { gameTypes } from "./index";
 
 export function LazyGameRenderer<G extends RuntimeGenerics>({
   type,
+  loader = (
+    <Center>
+      <LoadingIndicator />
+    </Center>
+  ),
   ...rendererProps
-}: GameRendererProps<G> & { type: GameTypeId }) {
+}: GameRendererProps<G> & { type: GameTypeId; loader?: ReactNode }) {
   const { renderer: Renderer } = gameTypes[type];
-  return <Renderer {...(rendererProps as ComponentProps<typeof Renderer>)} />;
+
+  return (
+    <Suspense fallback={loader}>
+      <Renderer {...(rendererProps as ComponentProps<typeof Renderer>)} />
+    </Suspense>
+  );
 }
