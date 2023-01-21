0d866ba4a4ffcfb62e6e298e0914a6006dde7d11
ksandin <kasper.sandin@gmail.com>
2023-01-22 00:32:10 +0100

    Clear game from editor store when leaving editor

--- src/app/features/editor/utils/StateSynchronizer.tsx
+++ src/app/features/editor/utils/StateSynchronizer.tsx
@@ -1,5 +1,6 @@
 import { isEqual } from "lodash";
 import { useDebounce } from "use-debounce";
+import { useEffect } from "react";
 import { trpc } from "../../../trpc";
 import { useToastProcedure } from "../../../hooks/useToastProcedure";
 import { useActions } from "../../../../lib/useActions";
@@ -16,17 +17,27 @@ export function StateSynchronizer({ gameId }: { gameId: GameId }) {
   const { data: remoteGame } = trpc.game.read.useQuery(gameId);
   const { mutate: upload } = useToastProcedure(trpc.game.update);
 
+  // Update local game when remote game changes
   useReaction(() => {
     if (remoteGame?.gameId === gameId) {
       setLocalGame(remoteGame);
     }
   }, [gameId, remoteGame]);
 
+  // Update remote game when local game changes
   useReaction(() => {
     if (debouncedLocalGame && !isEqual(debouncedLocalGame, remoteGame)) {
       upload(debouncedLocalGame);
     }
   }, [debouncedLocalGame]);
+
+  // Unset local game when unmounting
+  useEffect(
+    () => () => {
+      setLocalGame(undefined);
+    },
+    [setLocalGame]
+  );
 
   return null;
 }
