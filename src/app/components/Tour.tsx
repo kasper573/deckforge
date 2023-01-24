11c9e27d26c070838e3bfba74f969c7633085498
ksandin <kasper.sandin@gmail.com>
2023-01-24 21:13:53 +0100

    Animate editor tour tooltips
    Fixes #65

--- src/app/components/Tour.tsx
+++ src/app/components/Tour.tsx
@@ -6,7 +6,7 @@ import { styled } from "@mui/material/styles";
 
 // eslint-disable-next-line mui-path-imports/mui-path-imports
 import { Backdrop, Popper, Zoom } from "@mui/material";
-import { useEffect, useMemo } from "react";
+import { useEffect, useLayoutEffect, useMemo, useState } from "react";
 import useTheme from "@mui/material/styles/useTheme";
 import IconButton from "@mui/material/IconButton";
 import Tooltip from "@mui/material/Tooltip";
@@ -58,11 +58,21 @@ export function Tour({ steps, state, onChange }: TourProps) {
     }
   }, [anchor, active]);
 
+  // This is a hack to make the popper not transition positionally when fading in.
+  const [transition, setTransition] = useState(false);
+  useLayoutEffect(() => {
+    setTimeout(() => setTransition(active), 0);
+  }, [active]);
+
   return (
     <>
       <TooltipBackdrop style={{ clipPath }} open={active} />
       <Popper
         components={{ Root: TooltipRoot }}
+        componentsProps={{
+          // Assertion necessary because Popper has poorly defined types
+          root: { transition } as Record<string, unknown>,
+        }}
         open={active}
         anchorEl={virtualAnchor}
         placement="auto"
@@ -72,6 +82,11 @@ export function Tour({ steps, state, onChange }: TourProps) {
             {
               name: "offset",
               options: { offset: [0, parseInt(theme.spacing(2))] },
+            },
+            {
+              // Disable built in transition since we're adding a manual css transition
+              name: "computeStyles",
+              options: { adaptive: false },
             },
           ],
         }}
@@ -115,10 +130,14 @@ const TooltipCard = styled(Paper)`
   max-width: 500px;
 `;
 
-const TooltipRoot = styled("div")`
+const TooltipRoot = styled("div")<{ transition: boolean }>`
   z-index: ${({ theme }) => theme.zIndex.drawer - 1};
+  transition: ${({ theme, transition }) =>
+    transition ? theme.transitions.create("transform") : undefined};
 `;
 
 const TooltipBackdrop = styled(Backdrop)`
   z-index: ${({ theme }) => theme.zIndex.appBar - 1};
+  transition: ${({ theme }) => theme.transitions.create("clip-path")},
+    ${({ theme }) => theme.transitions.create("opacity")} !important;
 `;
