fd1afae984bf94a2c5c4c39545fbb08af591b85a
ksandin <kasper.sandin@gmail.com>
2023-02-01 21:25:02 +0100

    Simplify Header.tsx

--- src/app/features/layout/Header.tsx
+++ src/app/features/layout/Header.tsx
@@ -1,15 +1,10 @@
-import Breadcrumbs from "@mui/material/Breadcrumbs";
-import useTheme from "@mui/material/styles/useTheme";
 import type { ReactNode } from "react";
+import Typography from "@mui/material/Typography";
 
 export function Header({ children }: { children?: ReactNode }) {
-  const theme = useTheme();
   return (
-    <Breadcrumbs
-      role="heading"
-      sx={{ height: 24, mb: 2, ...theme.typography.h6 }}
-    >
-      {typeof children === "string" ? <span>{children}</span> : children}
-    </Breadcrumbs>
+    <Typography role="heading" color="lightgrey" variant="h6" paragraph>
+      {children}
+    </Typography>
   );
 }
