6767001b12f3fa6940dd6d88ea7599371820fd1d
ksandin <kasper.sandin@gmail.com>
2023-02-01 20:57:35 +0100

    Optimize typescript resolution in Header.tsx

--- src/app/features/layout/Header.tsx
+++ src/app/features/layout/Header.tsx
@@ -1,21 +1,13 @@
 import Breadcrumbs from "@mui/material/Breadcrumbs";
-import type { ComponentProps } from "react";
 import useTheme from "@mui/material/styles/useTheme";
+import type { ReactNode } from "react";
 
-export function Header({
-  children,
-  breadcrumbs = true,
-  sx,
-  ...props
-}: {
-  breadcrumbs?: boolean;
-} & Omit<ComponentProps<typeof Breadcrumbs>, "title">) {
+export function Header({ children }: { children?: ReactNode }) {
   const theme = useTheme();
   return (
     <Breadcrumbs
       role="heading"
-      sx={{ height: 24, mb: 2, ...theme.typography.h6, ...sx }}
-      {...props}
+      sx={{ height: 24, mb: 2, ...theme.typography.h6 }}
     >
       {typeof children === "string" ? <span>{children}</span> : children}
     </Breadcrumbs>
