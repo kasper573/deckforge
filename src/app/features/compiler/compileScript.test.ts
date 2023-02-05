045e61a77c8fbf103e06ed8a831cdd23a7f166d5
ksandin <kasper.sandin@gmail.com>
2023-02-05 01:02:56 +0100

    Test more cases

--- src/app/features/compiler/compileScript.test.ts
+++ src/app/features/compiler/compileScript.test.ts
@@ -1,5 +1,6 @@
 import type { ZodType } from "zod";
 import { z } from "zod";
+import type { CompileScriptResult } from "./compileScript";
 import { compileScript } from "./compileScript";
 import type { RuntimeScriptAPI } from "./types";
 
@@ -8,17 +9,37 @@ describe("can compile", () => {
     ["number", "123", z.number(), 123],
     ["string", "'abc'", z.string(), "abc"],
     ["boolean", "true", z.boolean(), true],
+    ["null", "null", z.null(), null],
+    ["undefined", "undefined", z.undefined(), undefined],
     [
-      "array",
-      "[1, `foo`, 3]",
-      z.tuple([z.number(), z.string(), z.number()]),
-      [1, "foo", 3],
+      "class",
+      `class Foo {
+        constructor (count) {
+          this.count = count;
+        }
+        next () { return ++this.count; } 
+      }`,
+      z.function(),
+      (Foo: new (n: number) => { next(): number }) => {
+        expect(Foo.name).toEqual("Foo");
+        const foo = new Foo(1);
+        foo.next();
+        expect(foo.next()).toEqual(3);
+      },
     ],
+    ["empty array", "[]", z.array(z.any()), []],
     [
-      "object",
-      "{ a: `foo`, b: 2 }",
-      z.object({ a: z.string(), b: z.number() }),
-      { a: "foo", b: 2 },
+      "array with values",
+      "[1, `foo`, () => 1]",
+      z.tuple([z.number(), z.string(), z.function()]),
+      [1, "foo", expect.any(Function)],
+    ],
+    ["empty object", "{ }", z.object({}), {}],
+    [
+      "object with properties",
+      "{ a: `foo`, b: 2, c () {} }",
+      z.object({ a: z.string(), b: z.number(), c: z.function() }),
+      { a: "foo", b: 2, c: expect.any(Function) },
     ],
     ["function", "(a, b) => a + b", z.function(), expect.any(Function)],
   ] as const;
@@ -33,16 +54,26 @@ describe("can compile", () => {
 function generateCompileScriptTest<T extends ZodType>(
   code: string,
   type: T,
-  expectation: unknown
+  expectation: ((value: z.infer<T>) => unknown) | unknown
 ) {
+  function assert(res: CompileScriptResult<T>) {
+    if (typeof expectation === "function") {
+      if (res.type === "error") {
+        throw res.error;
+      }
+      expectation(res.value as z.infer<T>);
+    } else {
+      expect(res).toEqual({ type: "success", value: expectation });
+    }
+  }
   it("using define", () => {
     const res = compileScript(`define(${code})`, { type, scriptAPI });
-    expect(res).toEqual({ type: "success", value: expectation });
+    assert(res);
   });
 
-  it.skip("using derive", () => {
+  it("using derive", () => {
     const res = compileScript(`derive(() => (${code}))`, { type, scriptAPI });
-    expect(res).toEqual({ type: "success", value: expectation });
+    assert(res);
   });
 }
 
