3679027ea524ce2004c526cc1589f9b68d8e3cff
ksandin <kasper.sandin@gmail.com>
2023-02-14 07:16:47 +0100

    Invoke without code generation

--- src/app/features/compiler/moduleRuntimes/QuickJS.ts
+++ src/app/features/compiler/moduleRuntimes/QuickJS.ts
@@ -52,6 +52,7 @@ class QuickJSModule<Definition extends ModuleDefinition = ModuleDefinition> {
   readonly compiled: CompiledModule<Definition["type"]>;
   private readonly globalsHandle?: RecursiveQuickJSHandle;
   private readonly vm: QuickJSContext;
+  private readonly definitionHandle?: QuickJSHandle;
   readonly error?: unknown;
 
   constructor(private runtime: QuickJSRuntime, private definition: Definition) {
@@ -60,9 +61,7 @@ class QuickJSModule<Definition extends ModuleDefinition = ModuleDefinition> {
       ? assign(this.vm, this.vm.global, definition.globals)
       : undefined;
 
-    const result = this.vm.evalCode(
-      `${defineFunctionConventionBindings}\n${definition.code}`
-    );
+    const result = this.vm.evalCode(defineCode(definition.code));
 
     if (result.error) {
       this.error = coerceError(
@@ -70,46 +69,56 @@ class QuickJSModule<Definition extends ModuleDefinition = ModuleDefinition> {
         `Failed to compile module "${definition.name}"`
       );
     } else {
-      result.value.dispose();
+      this.definitionHandle = result.value;
     }
 
-    this.compiled = createQuickJSModuleInterface(this.vm, this.definition);
+    this.compiled = createZodProxy(definition.type, (path, typeAtPath) => {
+      if (!zodInstanceOf(typeAtPath, ZodFunction)) {
+        throw new Error("Unsupported type");
+      }
+      return (...args: unknown[]) => this.call(path, args);
+    });
+  }
+
+  call(path: string[], args: unknown[]) {
+    const argNodes = args.map((arg) => newValue(this.vm, arg));
+    const argHandles = argNodes.map((n) => n.handle);
+    const result = this.vm.callFunction(
+      this.getHandleAtPath(path),
+      this.vm.null,
+      ...argHandles
+    );
+
+    argNodes.forEach(disposeRecursive);
+
+    if (result.error) {
+      throw coerceError(
+        result.error.consume(this.vm.dump),
+        "Failed to invoke " + [this.definition.name, ...path].join(".")
+      );
+    }
+
+    return result.value.consume(this.vm.dump);
+  }
+
+  getHandleAtPath(path: string[]): QuickJSHandle {
+    if (!this.definitionHandle) {
+      throw new Error("Module not compiled");
+    }
+    return path.reduce(
+      (acc, key) => this.vm.getProp(acc, key),
+      this.definitionHandle
+    );
   }
 
   dispose() {
+    this.definitionHandle?.dispose();
     if (this.globalsHandle) {
       disposeRecursive(this.globalsHandle);
     }
     this.vm.dispose();
   }
 }
-
-function createQuickJSModuleInterface<Def extends ModuleDefinition>(
-  vm: QuickJSContext,
-  { type, name }: Def
-): z.infer<Def["type"]> {
-  return createZodProxy(type, (path, typeAtPath) => {
-    if (!zodInstanceOf(typeAtPath, ZodFunction)) {
-      throw new Error("Unsupported type");
-    }
-    return (...args: unknown[]) => {
-      const result = vm.evalCode(invocationCode(path, args));
-      if (result.error) {
-        throw coerceError(
-          result.error.consume(vm.dump),
-          "Failed to invoke " + [name, ...path].join(".")
-        );
-      }
-      return result.value.consume(vm.dump);
-    };
-  });
-}
-
-const invocationCode = (path: string[], args: unknown[]) =>
-  `${symbols.select}(${JSON.stringify(path)})(${stringifyArgs(args)});`;
-
-const stringifyArgs = (args: unknown[]) =>
-  args.map((a) => JSON.stringify(a)).join(", ");
 
 function newValue(vm: QuickJSContext, value: unknown): RecursiveQuickJSHandle {
   if (Array.isArray(value)) {
@@ -148,6 +157,8 @@ function assign<Target extends QuickJSHandle>(
   target: Target,
   props: object
 ): RecursiveQuickJSHandle {
+  if (props instanceof ModuleReferences) {
+  }
   const children: RecursiveQuickJSHandle[] = [];
   for (const [k, v] of Object.entries(props)) {
     const node = newValue(vm, v);
@@ -182,18 +193,14 @@ function coerceError(input: unknown, description: string): Error {
   return new Error(description + ": " + String(input));
 }
 
-const symbols = {
-  ...abstractSymbols,
-  def: "___definition___",
-  select: "___selectFromDefinition___",
-};
-
-const defineFunctionConventionBindings = `
-let ${symbols.def} = undefined;
-function ${abstractSymbols.define} (def) {
-  ${symbols.def} = def;
+function defineCode(code: string) {
+  const def = "___definition___";
+  return `
+    let ${def} = undefined;
+    function ${abstractSymbols.define} (def) {
+      ${def} = def;
+    }
+    (() => { ${code} } )();
+    ${def}
+  `;
 }
-function ${symbols.select}(path) {
-  return path.reduce((acc, key) => acc[key], ${symbols.def});
-}
-`;
