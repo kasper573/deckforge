cf22da727cf7dbf40f45b4a7a6e1174d8fc4c2c9
ksandin <kasper.sandin@gmail.com>
2023-02-15 18:53:07 +0100

    jsInterpreter should be es5, and quickJS should be es6

--- src/app/features/compiler/moduleRuntimes/JSInterpreter/index.ts
+++ src/app/features/compiler/moduleRuntimes/JSInterpreter/index.ts
@@ -2,7 +2,7 @@ import type { ModuleCompilerInfo } from "../types";
 
 export const jsInterpreterCompilerInfo: ModuleCompilerInfo = {
   name: "JSInterpreter",
-  tsCompilerOptions: { lib: ["ES5"] },
+  tsCompilerOptions: { lib: ["es5"] },
   loadCompilerFactory: async () => {
     const { JSInterpreterCompiler } = await import("./JSInterpreter");
     return () => new JSInterpreterCompiler();
--- src/app/features/compiler/moduleRuntimes/QuickJS/index.ts
+++ src/app/features/compiler/moduleRuntimes/QuickJS/index.ts
@@ -2,7 +2,7 @@ import type { ModuleCompilerInfo } from "../types";
 
 export const quickJSCompilerInfo: ModuleCompilerInfo = {
   name: "QuickJS",
-  tsCompilerOptions: { lib: ["ES2020"] },
+  tsCompilerOptions: { lib: ["es6"] },
   loadCompilerFactory: async () => {
     const [quickJS, { createQuickJSCompiler }] = await Promise.all([
       import("quickjs-emscripten").then(({ getQuickJS }) => getQuickJS()),
