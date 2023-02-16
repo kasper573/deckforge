567c4339f93d0b8cdb6fc6855add59dc8e89831d
ksandin <kasper.sandin@gmail.com>
2023-02-16 08:19:41 +0100

    Add runtime limits to quick js compiler

--- src/app/features/compiler/moduleRuntimes/QuickJS/index.ts
+++ src/app/features/compiler/moduleRuntimes/QuickJS/index.ts
@@ -1,3 +1,4 @@
+import type { InterruptHandler } from "quickjs-emscripten";
 import type { ModuleCompilerInfo } from "../types";
 
 export const quickJSCompilerInfo: ModuleCompilerInfo = {
@@ -8,6 +9,32 @@ export const quickJSCompilerInfo: ModuleCompilerInfo = {
       import("quickjs-emscripten").then(({ getQuickJS }) => getQuickJS()),
       import("./QuickJSCompiler"),
     ]);
-    return () => createQuickJSCompiler(() => quickJS.newRuntime());
+    return () =>
+      createQuickJSCompiler(() =>
+        quickJS.newRuntime({
+          memoryLimitBytes: 1024 * 640,
+          maxStackSizeBytes: 1024 * 320,
+          interruptHandler: createInterruptHandler(),
+        })
+      );
   },
 };
+
+function createInterruptHandler({
+  stallThreshold = 16,
+  maxStallTime = 1000,
+} = {}): InterruptHandler {
+  let lastInterruptTime = Date.now();
+  let accumulatedStallTime = 0;
+  return () => {
+    const now = Date.now();
+    const delta = now - lastInterruptTime;
+    lastInterruptTime = now;
+    if (delta <= stallThreshold) {
+      accumulatedStallTime += delta;
+    } else {
+      accumulatedStallTime = 0;
+    }
+    return accumulatedStallTime >= maxStallTime;
+  };
+}
