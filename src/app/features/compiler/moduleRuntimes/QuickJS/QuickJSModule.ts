cc28f891b47d2314740bc03b9c2dec36cbffaa17
ksandin <kasper.sandin@gmail.com>
2023-02-16 06:15:35 +0100

    . -> :

--- src/app/features/compiler/moduleRuntimes/QuickJS/QuickJSModule.ts
+++ src/app/features/compiler/moduleRuntimes/QuickJS/QuickJSModule.ts
@@ -118,7 +118,7 @@ function coerceError(input: unknown, description: string): Error {
   const result = errorType.safeParse(input);
   if (result.success) {
     const { name, message, stack } = result.data;
-    return new Error(`${description}. ${name}: ${message}\n${stack}`);
+    return new Error(`${description}: ${name}: ${message}\n${stack}`);
   }
   return new Error(description + ": " + String(input));
 }
