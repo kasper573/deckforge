5037705711109be28a8dd9f79d204f4ca322dd5d
ksandin <kasper.sandin@gmail.com>
2023-02-16 07:28:20 +0100

    Improved mutation function

--- src/app/features/compiler/moduleRuntimes/createMutateFn.ts
+++ src/app/features/compiler/moduleRuntimes/createMutateFn.ts
@@ -1,27 +1,50 @@
 export function createMutateFn() {
   return function mutate(a: unknown, b: unknown) {
-    if (Array.isArray(a) && Array.isArray(b)) {
-      const maxLength = Math.max(a.length, b.length);
-      for (let i = 0; i < maxLength; i++) {
-        a[i] = mutate(a[i], b[i]);
-      }
-      return a;
+    if (!(isObject(a) && isObject(b))) {
+      // A or B is a primitive, which means A can't be mutated.
+      // B is the new value, returning leads to a mutation during recursion.
+      return b;
     }
-    if (isObject(a) && isObject(b)) {
-      for (const key of Object.keys(b)) {
-        const nextValue = mutate(a[key], b[key]);
-        try {
-          a[key] = nextValue;
-        } catch {
-          // ignore read-only errors
+
+    const reverseKeys = unique([
+      ...Object.keys(a),
+      ...Object.keys(b),
+    ]).reverse();
+
+    const targetIsArray = Array.isArray(a);
+
+    for (const key of reverseKeys) {
+      const removed = !(key in b);
+      const added = !(key in a);
+      const mutated = !removed && !added;
+      if (removed) {
+        delete a[key];
+        if (targetIsArray) {
+          a.splice(Number(key), 1);
+        }
+      } else if (added) {
+        a[key] = b[key];
+      } else if (mutated) {
+        const newValue = mutate(a[key], b[key]);
+        if (newValue !== a[key]) {
+          a[key] = newValue;
         }
       }
-      return a;
     }
-    return b;
+    return a;
   };
 
-  function isObject(obj: unknown): obj is Record<string, unknown> {
-    return obj !== null && typeof obj === "object";
+  function isObject(value: unknown): value is Record<string, unknown> {
+    return value !== null && typeof value === "object";
+  }
+
+  function unique<T>(array: T[]): T[] {
+    const seen: T[] = [];
+    for (const item of array) {
+      if (seen.indexOf(item) === -1) {
+        seen.push(item);
+      }
+    }
+    return seen;
   }
 }
