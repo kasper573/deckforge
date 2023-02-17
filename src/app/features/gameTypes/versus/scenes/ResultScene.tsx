6adb5a8b1fccad344e7ca364fc6bf290fc4030ec
ksandin <kasper.sandin@gmail.com>
2023-02-17 20:02:57 +0100

    Refactor

--- src/app/features/gameTypes/versus/scenes/ResultScene.tsx
+++ src/app/features/gameTypes/versus/scenes/ResultScene.tsx
@@ -4,19 +4,15 @@ import type { RuntimePlayerId } from "../../../compiler/types";
 import { adapter } from "../runtimeDefinition";
 import { Center } from "../../../../components/Center";
 
-export function ResultScene({ winner }: { winner: RuntimePlayerId }) {
+export function ResultScene({ winner: winnerId }: { winner: RuntimePlayerId }) {
   const players = adapter.useRuntimeState((state) => state.players);
   const { restartGame } = adapter.useRuntimeActions();
-
-  function getPlayerName(id: RuntimePlayerId) {
-    const index = players.findIndex((p) => p.id === id);
-    return `Player ${index + 1}`;
-  }
+  const winner = players.find((p) => p.id === winnerId);
 
   return (
     <Center>
       <Typography sx={{ textAlign: "center", color: "black" }}>
-        {getPlayerName(winner)} won!
+        {winner?.properties.name} won!
       </Typography>
       <Button variant="contained" onClick={restartGame} sx={{ mt: 2 }}>
         Play again
