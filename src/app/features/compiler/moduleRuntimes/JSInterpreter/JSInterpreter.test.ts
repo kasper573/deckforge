45ac226ca625b7c0a7d6385ce385e5d344744769
ksandin <kasper.sandin@gmail.com>
2023-02-25 00:28:44 +0100

    Remove js-interpreter

--- src/app/features/compiler/moduleRuntimes/JSInterpreter/JSInterpreter.test.ts
+++
@@ -1,6 +0,0 @@
-import { generateModuleRuntimeTests } from "../tests";
-import { JSInterpreterCompiler } from "./JSInterpreter";
-
-describe("JSInterpreter", () => {
-  generateModuleRuntimeTests(() => new JSInterpreterCompiler());
-});
