65c2d8fc2e2ed89293dcb73c34e41b3ca6c4ef14
ksandin <kasper.sandin@gmail.com>
2023-02-24 21:36:55 +0100

    Expose runtime and always dispose

--- src/app/features/gameTypes/testUtils.ts
+++ src/app/features/gameTypes/testUtils.ts
@@ -1,13 +1,12 @@
 import { getQuickJS } from "quickjs-emscripten";
-import type { CompiledGame } from "../compiler/compileGame";
 import { compileGame } from "../compiler/compileGame";
 import { createQuickJSCompiler } from "../compiler/moduleRuntimes/QuickJS/QuickJSCompiler";
-import type { RuntimeGenerics } from "../compiler/types";
+import type { GameRuntime, RuntimeGenerics } from "../compiler/types";
 import type { GameType } from "./GameType";
 
 export async function testGameType<G extends RuntimeGenerics>(
   gameType: GameType<G>,
-  test: (game: CompiledGame<G>) => void
+  test: (runtime: GameRuntime<G>) => void
 ) {
   const quickJS = await getQuickJS();
   const gameDefinition = await gameType.defaultGameDefinition();
@@ -20,7 +19,10 @@ export async function testGameType<G extends RuntimeGenerics>(
   expect(result).toEqual(expect.objectContaining({ value: expect.anything() }));
 
   if (result.isOk()) {
-    await test(result.value);
-    result.value.dispose();
+    try {
+      test(result.value.runtime);
+    } finally {
+      result.value.dispose();
+    }
   }
 }
